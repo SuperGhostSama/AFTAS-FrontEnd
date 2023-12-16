36c3bc90d38d850cc159d392618d6359e22f69ff
“SuperGhost” <salaheddinehanaoui@gmail.com>
2023-12-16 20:44:13 +0100

    consumed the competition endpoints in the service

--- aftas/src/app/services/Competition/competition.service.ts
+++ aftas/src/app/services/Competition/competition.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CompetitionService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getCompetitionById(id: number): Observable<any> {
+    return this.http.get(`${environment.competitionsApi}/${id}`);
+  }
+
+  getAllCompetitions(): Observable<any> {
+    return this.http.get(environment.competitionsApi);
+  }
+
+  addCompetition(competitionRequest: any): Observable<any> {
+    return this.http.post(environment.competitionsApi, competitionRequest);
+  }
+
+  updateCompetition(competitionRequest: any, id: number): Observable<any> {
+    return this.http.put(`${environment.competitionsApi}/${id}`, competitionRequest);
+  }
+
+  deleteCompetition(id: number): Observable<any> {
+    return this.http.delete(`${environment.competitionsApi}/${id}`);
+  }
 }
