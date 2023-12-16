a59edabfe3aa96c395062e610b4701f5e9fd60bc
“SuperGhost” <salaheddinehanaoui@gmail.com>
2023-12-16 20:46:47 +0100

    consumed the members endpoints in the service

--- aftas/src/app/services/Member/member.service.ts
+++ aftas/src/app/services/Member/member.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MemberService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getAllMembers(): Observable<any> {
+    return this.http.get(`${environment.membersApi}`);
+  }
+
+  getMemberById(id: number): Observable<any> {
+    return this.http.get(`${environment.membersApi}/${id}`);
+  }
+
+  addMember(member: any): Observable<any> {
+    return this.http.post(`${environment.membersApi}`, member);
+  }
+
+  updateMember(member: any, id: number): Observable<any> {
+    return this.http.put(`${environment.membersApi}/${id}`, member);
+  }
+
+  deleteMember(id: number): Observable<any> {
+    return this.http.delete(`${environment.membersApi}/${id}`);
+  }
+
+  searchMember(name: string): Observable<any> {
+    return this.http.get(`${environment.membersApi}/search?name=${name}`);
+  }
 }
