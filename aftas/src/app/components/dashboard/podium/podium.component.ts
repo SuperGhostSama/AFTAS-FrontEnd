d813eec5e418f0f155fa0a92aa7d66358d256296
“SuperGhost” <salaheddinehanaoui@gmail.com>
2023-12-17 18:13:21 +0100

    finished showing the rankings of a precise competition

--- aftas/src/app/components/dashboard/podium/podium.component.ts
+++ aftas/src/app/components/dashboard/podium/podium.component.ts
@@ -4,6 +4,7 @@ import { Subscription } from 'rxjs';
 import { MemberService } from 'src/app/services/Member/member.service';
 import { RankingService } from 'src/app/services/Ranking/ranking.service';
 
+declare var $: any;
 @Component({
   selector: 'app-podium',
   templateUrl: './podium.component.html',
@@ -13,6 +14,7 @@ export class PodiumComponent {
   private subscription!: Subscription;
   selectedCompetitionId: string = '';
   membersRanking: any[] = [];
+  dataTable: any;
 
 
   constructor(
@@ -37,8 +39,7 @@ export class PodiumComponent {
       (data) => {
         this.membersRanking = data;
         console.log("This is the members Ranking for podium :",this.membersRanking);
-        // Call a method to rerender the podium HTML
-        this.renderThePodium();
+        this.refreshDataTable();
       },
       (error) => {
         console.error('Error fetching members by competition ID:', error);
@@ -46,58 +47,25 @@ export class PodiumComponent {
     );
   }
 
-
-  renderThePodium(): void {
-    // Get the podiumDiv element
-    const podiumDiv = document.getElementById('podiumDiv');
-  
-    // Check if podiumDiv is not null
-    if (podiumDiv) {
-      // Clear the existing content
-      podiumDiv.innerHTML = '';
-  
-      // Create a new container div for the podium
-      const containerDiv = document.createElement('div');
-      containerDiv.className = 'container podium justify-content-center mt-5';
-  
-      // Iterate through membersRanking and create podium__item divs
-      this.membersRanking.forEach((member, index) => {
-        const podiumItemDiv = document.createElement('div');
-        podiumItemDiv.className = 'podium__item';
-  
-        // Create and set the HTML for podium__city
-        const podiumCityP = document.createElement('p');
-        podiumCityP.className = 'podium__city';
-        podiumCityP.textContent = member.name;
-        podiumItemDiv.appendChild(podiumCityP);
-  
-        // Create and set the HTML for podium__rank
-        const podiumRankDiv = document.createElement('div');
-        podiumRankDiv.className = `podium__rank ${this.getRankClass(index)}`;
-        podiumRankDiv.textContent = (index + 1).toString();
-        podiumItemDiv.appendChild(podiumRankDiv);
-  
-        // Append the podium__item div to the container
-        containerDiv.appendChild(podiumItemDiv);
-      });
-  
-      // Append the containerDiv to the podiumDiv
-      podiumDiv.appendChild(containerDiv);
+  refreshDataTable(): void {
+    if (!this.dataTable) {
+      this.initializeDataTable();
+    } else {
+      this.dataTable.clear();
+      this.dataTable.rows.add(this.membersRanking);
+      this.dataTable.draw();
     }
   }
-  
-  // Helper function to determine the rank class based on index
-  getRankClass(index: number): string {
-    switch (index) {
-      case 0:
-        return 'first';
-      case 1:
-        return 'second';
-      case 2:
-        return 'third';
-      default:
-        return '';
-    }
+ 
+  initializeDataTable(): void {
+    this.dataTable = $('#podiumTable').DataTable({
+      data: this.membersRanking,
+      columns: [
+        { data: 'rank', title: 'Rank', className: 'text-center fw-bold' },
+        { data: 'memberName', title: 'Member Name', className: 'text-center fw-bold' },
+        { data: 'score', title: 'Score', className: 'text-center' }
+      ],
+    });
   }
   
 }
